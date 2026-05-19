cc291180369f6b4be6f53f55a589b7561d106079
Mark Johnson <markj@matzsoft.com>
2026-05-18 17:53:38 -0700

    Added paths.swift and errors.swift.

---
+++ Sources/MATZMisc/paths.swift
@@ -0,0 +1,9 @@
+//         FILE: paths.swift
+//  DESCRIPTION: MATZMisc - ---
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2026 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 5/18/26 5:07 PM
+
+import Foundation
